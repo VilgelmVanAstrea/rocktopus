6b23736fa6196b67f5dc934ee3525cf4ceea30e5
VilgelmVanAstrea <il.safronov2015@yandex.ru>
2023-06-18 19:19:58 +0400

    Add capital_letter_up

--- str_func.py
+++ str_func.py
@@ -1,11 +1,14 @@
 def letter_up(word):
+    """
+    Dockstring
+
+    """
     return word.upper()
 
 
 def capital_letter_up(word):
     """
     Dockstring
-    :param word:
-    :return:
+
     """
     return word.title()
